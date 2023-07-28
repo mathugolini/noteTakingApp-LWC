07782f0800635058f5a0cdc314e94f0cb03b2205
Matheus <mathugolini@gmail.com>
2023-07-28 13:59:26 -0300

    Implementação do modal

--- force-app/main/default/lwc/noteTakingApp/noteTakingApp.js
+++ force-app/main/default/lwc/noteTakingApp/noteTakingApp.js
@@ -1,3 +1,13 @@
 import { LightningElement } from 'lwc';
 
-export default class NoteTakingApp extends LightningElement {}
+export default class NoteTakingApp extends LightningElement {
+    showModal = false
+
+    createNoteHandler() {
+        this.showModal = true
+    }
+    
+    closeModalHandler() {
+        this.showModal = false
+    }
+}
