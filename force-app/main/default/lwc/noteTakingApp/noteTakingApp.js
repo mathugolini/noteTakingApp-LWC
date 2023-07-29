5cf17dd4c8a140271ed89542962838f028dd3d76
Matheus <mathugolini@gmail.com>
2023-07-29 18:15:11 -0300

    Atualizando projeto-removendo erro

--- force-app/main/default/lwc/noteTakingApp/noteTakingApp.js
+++ force-app/main/default/lwc/noteTakingApp/noteTakingApp.js
@@ -1,64 +1,67 @@
 import { LightningElement } from 'lwc';
 import createNoteRecord from '@salesforce/apex/NoteTakingController.createNoteRecord'
-
 const DEFAULT_NOTE_FORM = {
-    Name: "",
-    Note_Description__c:""
+  Name:"",
+  Note_Description__c:""
 }
 export default class NoteTakingApp extends LightningElement {
-    showModal = false
-    noteRecord = DEFAULT_NOTE_FORM
+  showModal = false
+  noteRecord = DEFAULT_NOTE_FORM
+  formats = [
+    'font',
+    'size',
+    'bold',
+    'italic',
+    'underline',
+    'strike',
+    'list',
+    'indent',
+    'align',
+    'link',
+    'clean',
+    'table',
+    'header',
+    'color',
+];
 
-    formats = [
-        'font',
-        'size',
-        'bold', 
-        'italic',
-        'underline',
-        'strike',
-        'list',
-        'indent',
-        'align',
-        'link',
-        'clean',
-        'table',
-        'header',
-        'color'
-    ];
+get isFormInvalid(){
+  return !(this.noteRecord && this.noteRecord.Note_Description__c && this.noteRecord.Name)
+}
 
-    get isFormInvalid() {
-        return !(this.noteRecord && this.noteRecord.Note_Description__c && this.noteRecord.Name)
-    }
+  createNoteHandler(){
+    this.showModal = true
+  }
+  closeModalHandler(){
+    this.showModal = false
+    this.noteRecord = DEFAULT_NOTE_FORM
+  }
 
-    createNoteHandler() {
-        this.showModal = true
-    }
-    
-    closeModalHandler() {
-        this.showModal = false
-        this.noteRecord = DEFAULT_NOTE_FORM
-    }
+  changeHandler(event){
+    const {name, value} = event.target
+    // const name = event.target.name
+    // const value = event.target.value
+    this.noteRecord={...this.noteRecord, [name]:value}
+  }
+  formSubmitHandler(event){
+    event.preventDefault();
+    console.log("this.noteRecord", JSON.stringify(this.noteRecord))
+    this.createNote()
+  }
 
-    changeHandler(event) {
-        const {name, value} = event.target
-        this.noteRecord={...this.noteRecord, [name]:value}
-    }
+  createNote(){
+    createNoteRecord({title:this.noteRecord.Name, description:this.noteRecord.Note_Description__c}).then(()=>{
+      this.showModal = false;
+      this.showToastMsg("Note Created Successfully!!", 'success')
+    }).catch(error=>{
+      console.error("error", error.message.body)
+      this.showToastMsg(error.message.body, 'error')
+    })
+  }
 
-    formSubmitHandler(event) {
-        event.preventDefault();
-        console.log("this.noteRecord", JSON.stringify(this.noteRecord))
-        const elem = this.template.querySelector('c-notification')
-        if (elem) {
-            elem.showToast()
-        }
-        //this.createNote()
+  showToastMsg(message, variant){
+    const elem = this.template.querySelector('c-notification')
+    if(elem){
+      elem.showToast(message, variant)
     }
-
-    createNote() {
-        createNoteRecord({title:this.noteRecord.Name, description:this.noteRecord.Note_Description__c}).then(()=>{
-            this.showModal = false;
-        }).catch(error=>{
-            console.error("error", error.message.body)
-        })
-    }
+  }
 }
--- force-app/main/default/lwc/notification/notification.js
+++ force-app/main/default/lwc/notification/notification.js
@@ -1,23 +1,21 @@
 import { LightningElement, api } from 'lwc';
 
 export default class Notification extends LightningElement {
-
-    showNotification = false 
+    showNotification = false
     message
     variant
 
-    get notifyClassses() {
-        let variantClass = this.variant === 'success' ? 'slds-theme_success': 
-        this.variant === 'warning' ? 'slds-theme_warning': 
+    get notifyClasses(){
+        let variantClass = this.variant === 'success' ? 'slds-theme_success':
+        this.variant === 'warning' ? 'slds-theme_warning':
         this.variant === 'error' ? 'slds-theme_error':'slds-theme_info'
         return `slds-notify slds-notify_toast ${variantClass}`
     }
-
-    @api showToast(message, varant) {
+    @api showToast(message, variant){
         this.message = message || "Please pass your message"
         this.variant = variant || 'success'
         this.showNotification = true
-        setTimeout(()=> {
+        setTimeout(()=>{
             this.showNotification = false
         },5000 )
     }
