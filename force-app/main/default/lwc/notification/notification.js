ae1c121714e481b5b813bfa87c7e67e88ade9432
Matheus <mathugolini@gmail.com>
2023-07-29 17:57:37 -0300

    função notifyClasses e showToast implementadas

--- force-app/main/default/lwc/notification/notification.js
+++ force-app/main/default/lwc/notification/notification.js
@@ -3,8 +3,19 @@ import { LightningElement, api } from 'lwc';
 export default class Notification extends LightningElement {
 
     showNotification = false 
+    message
+    variant
 
-    @api showToast() {
+    get notifyClassses() {
+        let variantClass = this.variant === 'success' ? 'slds-theme_success': 
+        this.variant === 'warning' ? 'slds-theme_warning': 
+        this.variant === 'error' ? 'slds-theme_error':'slds-theme_info'
+        return `slds-notify slds-notify_toast ${variantClass}`
+    }
+
+    @api showToast(message, varant) {
+        this.message = message || "Please pass your message"
+        this.variant = variant || 'success'
         this.showNotification = true
         setTimeout(()=> {
             this.showNotification = false
